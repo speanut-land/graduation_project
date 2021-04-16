3b4f8f745c423b5c85b1413e124174128e3ea458
panghaitao <speanutcash@gmail.com>
2021-04-16 16:38:48 +0800

    feat: 删除部分代码

--- build/vite/plugin/html.ts
+++ build/vite/plugin/html.ts
@@ -2,20 +2,20 @@
  * Plugin to minimize and use ejs template syntax in index.html.
  * https://github.com/anncwb/vite-plugin-html
  */
-import type { Plugin } from 'vite';
+import type { Plugin } from "vite";
 
-import html from 'vite-plugin-html';
+import html from "vite-plugin-html";
 
-import pkg from '../../../package.json';
-import { GLOB_CONFIG_FILE_NAME } from '../../constant';
+import pkg from "../../../package.json";
+import { GLOB_CONFIG_FILE_NAME } from "../../constant";
 
 export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
   const { VITE_GLOB_APP_TITLE, VITE_PUBLIC_PATH } = env;
 
-  const path = VITE_PUBLIC_PATH.endsWith('/') ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
+  const path = VITE_PUBLIC_PATH.endsWith("/") ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
 
   const getAppConfigSrc = () => {
-    return `${path || '/'}${GLOB_CONFIG_FILE_NAME}?v=${pkg.version}-${new Date().getTime()}`;
+    return `${path || "/"}${GLOB_CONFIG_FILE_NAME}?v=${pkg.version}-${new Date().getTime()}`;
   };
 
   const htmlPlugin: Plugin[] = html({
@@ -29,7 +29,7 @@ export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
       tags: isBuild
         ? [
             {
-              tag: 'script',
+              tag: "script",
               attrs: {
                 src: getAppConfigSrc(),
               },
